0607bde9b6821cd1424dbc3b35161a5dfef8edd6
fredricklitvin <fredlitvin@gmail.com>
2026-01-03 14:45:14 +0000

    repo starting position working on infra

---
+++ infra/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.14.1"
+    }
+  }
+  backend "gcs" {
+    bucket = "terraform-infra-vault"
+    prefix = "terraform/state"
+  }
+  
+}
+
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+}
